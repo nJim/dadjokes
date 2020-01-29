27f2dd49960b7281f06f6699b0fb9bdfcfde6dd6
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-29 12:54:03 -0500

    Writing content for the about page.

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -6,18 +6,26 @@ function AboutPage() {
   const cards = [
     {
       title: 'React',
-      description: 'details'
+      description: 'JavaScript library used for building user interfaces. My components are organized using Atomic Design principals.'
     },
     {
       title: 'NextJS',
-      description: 'details'
+      description: 'Framework based on Vue.js, Node.js, Webpack and Babel.js. I\'m enjoying the server side rendering support.'
     },
     {
-      title: 'AntD',
-      description: 'details'
+      title: 'Ant Design',
+      description: 'React library with a deep set of components, layouts, and building tools. It\'s written in TypeScript with predictable static types.'
     },
     {
       title: 'Styled Components',
+      description: 'Library for writting CSS-in-JS. Includes features for passing props, organizing themes, and dynamically changing elements.'
+    },
+    {
+      title: 'Storybook',
+      description: 'Development tool that creates a playground for components. Includes addons for validating, testing, and interacting with components.'
+    },
+    {
+      title: 'TypeScript',
       description: 'details'
     },
     {
