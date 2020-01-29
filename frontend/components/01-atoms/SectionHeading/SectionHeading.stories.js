e6f1e403b5bfbba85c455ac881d96e7e8a6979f0
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-29 12:26:00 -0500

    Adding storybook and a single story.

---
+++ frontend/components/01-atoms/SectionHeading/SectionHeading.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SectionHeading from './';
+
+export default {
+  title: 'Section Heading',
+};
+
+export const heading = () => (
+  <SectionHeading>This is a section</SectionHeading>
+);
