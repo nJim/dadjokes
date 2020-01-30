3c19a3474168614bbda988631f634848e2b72797
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-30 17:15:05 -0500

    Stubbing out some action button state.

--- frontend/components/02-molecules/Action/DislikeAction.tsx
+++ frontend/components/02-molecules/Action/DislikeAction.tsx
@@ -1,13 +1,18 @@
+import {useState} from 'react';
 import {Tooltip} from 'antd';
 import {DislikeIcon} from '../../01-atoms/Icons';
 
 const DislikeAction = () => {
   // TODO: Make the counter a dynamic property.
   const counter = 1;
+
+  // Initiall set the active state to false and toggle on click.
+  const [active, setActive] = useState(false);
+  const toggleActive = () => setActive(value => !value);
   return (
-    <span>
+    <span onClick={toggleActive}>
       <Tooltip title="Dislike">
-        <DislikeIcon />
+        <DislikeIcon active={active} />
       </Tooltip>
       <span style={{ paddingLeft: 8, cursor: 'auto' }}>{counter}</span>
     </span>
--- frontend/components/02-molecules/Action/LikeAction.tsx
+++ frontend/components/02-molecules/Action/LikeAction.tsx
@@ -1,13 +1,18 @@
+import {useState} from 'react';
 import {Tooltip} from 'antd';
 import {LikeIcon} from '../../01-atoms/Icons';
 
 const LikeAction = () => {
   // TODO: Make the counter a dynamic property.
   const counter = 10;
+
+  // Initiall set the active state to false and toggle on click.
+  const [active, setActive] = useState(false);
+  const toggleActive = () => setActive(value => !value);
   return (
-    <span>
+    <span onClick={toggleActive}>
       <Tooltip title="Like">
-        <LikeIcon />
+        <LikeIcon active={active} />
       </Tooltip>
       <span style={{ paddingLeft: 8, cursor: 'auto' }}>{counter}</span>
     </span>
