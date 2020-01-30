fa8e6ff986721b96615532b52ce93f6361db6da0
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-30 00:58:33 -0500

    Setting up apollo and wrapping the app in a provider.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,30 @@
 import App from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 import Page from '../components/04-templates/Page';
 import '../components/00-base/global.css';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // This exposes the query to the user.
+    pageProps.query = ctx.query;
+    return { pageProps }; 
+  }
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
