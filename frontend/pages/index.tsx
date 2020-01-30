8bb5f93ab3815f269b876fa3cb9b2f796c4e8542
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-30 11:36:25 -0500

    Adding a query and dumping the contents on the home page.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,43 @@
+import React, {Component} from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const ALL_JOKES_QUERY = gql`
+  query ALL_JOKES_QUERY {
+    jokes {
+      id
+      content
+    }
+  }
+`;
+
+interface Data {
+  jokes: {
+    joke: Array<{ id: string; content: string }>;
+  };
+};
+
+const JokeList = () => {
+  const { loading, error, data } = useQuery(ALL_JOKES_QUERY);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    data.jokes.map((joke: { id: string; content: string; }) => (
+      <p key={joke.id}>{joke.content}</p>
+    ))
+  );
+
+}
+
+
 function HomePage() {
-  return <div>Welcome home.</div>
+  return (
+    <div>
+      Welcome home.
+      <JokeList />
+    </div>
+  );
 }
 
 export default HomePage;
