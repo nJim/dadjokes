0fe7c0cb60a12080cb2e651dfccb901f4ee583e6
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-30 16:22:48 -0500

    Adding action buttons to the jokes.

--- frontend/components/02-molecules/Joke/Joke.tsx
+++ frontend/components/02-molecules/Joke/Joke.tsx
@@ -2,10 +2,39 @@ import { Comment, Icon, Tooltip, Avatar } from 'antd';
 import {JokeData} from '../../../types'
 
 const Joke = ({id, content}: JokeData) => {
+  const likes = 10;
+  const dislikes = 1;
+
+  const actions = [
+    <span>
+      <Tooltip title="Like">
+        <Icon
+          type="like"
+          // theme={action === 'liked' ? 'filled' : 'outlined'}
+        />
+      </Tooltip>
+      <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
+    </span>,
+    <span>
+      <Tooltip title="Dislike">
+        <Icon
+          type="dislike"
+          // theme={action === 'disliked' ? 'filled' : 'outlined'}
+        />
+      </Tooltip>
+      <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
+    </span>,
+    <span>
+    <Tooltip title="Share">
+      <Icon type="share-alt" />
+    </Tooltip>
+  </span>
+  ];
+
   return(
     <Comment
       key={id}
-      // actions={actions}
+      actions={actions}
       author={<a>Han Solo</a>}
       avatar={
         <Avatar
