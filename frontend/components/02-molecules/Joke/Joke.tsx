42eaabd17658d0a4cd32cf91e6dcc6787300e884
Jim Vomero <jim.vomero@fourkitchens.com>
2020-01-30 16:07:05 -0500

    Using antd Comment component to style jokes.

--- frontend/components/02-molecules/Joke/Joke.tsx
+++ frontend/components/02-molecules/Joke/Joke.tsx
@@ -1,6 +1,22 @@
+import { Comment, Icon, Tooltip, Avatar } from 'antd';
 import {JokeData} from '../../../types'
 
-const Joke = ({id, content}: JokeData) => 
-  <p key={id}>{content}</p>;
+const Joke = ({id, content}: JokeData) => {
+  return(
+    <Comment
+      key={id}
+      // actions={actions}
+      author={<a>Han Solo</a>}
+      avatar={
+        <Avatar
+          src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+          alt="Han Solo"
+        />
+      }
+      content={content}
+      datetime={<span>Five minutes ago.</span>}
+    />
+  );
+} 
 
 export default Joke;
